04b3760ac24b938ced3416d6b8f5d5f62de37b95
Josh O'Brian <joshuaeobrian@gmail.com>
2017-03-28 16:42:05 -0400

    Second commit

---
+++ test/com/tiy/MenuServiceTest.java
@@ -0,0 +1,54 @@
+package com.tiy;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PrintStream;
+import java.time.LocalDate;
+import static org.hamcrest.CoreMatchers.*;
+import static org.hamcrest.MatcherAssert.*;
+
+/**
+ * Created by josh on 3/28/17.
+ */
+public class MenuServiceTest {
+	private ByteArrayOutputStream outputStream;
+	@Before
+	public void before(){
+		this.outputStream = new ByteArrayOutputStream();
+		PrintStream printStream = new PrintStream(outputStream);
+		System.setOut(printStream);
+	}
+
+	@Test
+	public void testMenuServiceConstructor(){
+
+	}
+
+	@Test
+	public void testWaitForInt(){
+
+	}
+
+	@Test
+	public void testWaitForString(){
+
+	}
+
+	@Test
+	public void checkYesOrNo(){
+
+	}
+	@Test
+	public void testPromptForMenu(){
+
+	}
+
+
+
+
+
+}
