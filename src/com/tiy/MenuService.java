5e340e41b804a72032d069b605fd6f18f2e10c30
Josh O'Brian <joshuaeobrian@gmail.com>
2017-03-29 13:49:10 -0400

    added more testing and function to menuService

--- src/com/tiy/MenuService.java
+++ src/com/tiy/MenuService.java
@@ -12,10 +12,30 @@ import java.util.Scanner;
 public class MenuService {
 
 
+	public static final int LIST_ANIMALS = 1;
+	public static final int CREATE_ANIMAL = 2;
+	public static final int VIEW_ANIMAL = 3;
+	public static final int EDIT_ANIMAL = 4;
+	public static final int DELETE_ANIMAL = 5;
+	public static final int QUIT = 6;
+	private String textAlign = " %-15s  %-9s %n";
+
 	private Scanner scanner;
 
 	public MenuService(Scanner scanner) {
 		this.scanner = scanner;
+	}
+
+	public int promptForMainMenu(){
+		System.out.println("\n-- Main Menu --\n" +
+				"\n" +
+				" 1) List animals\n" +
+				" 2) Create an animal\n" +
+				" 3) View animal details\n" +
+				" 4) Edit an animal\n" +
+				" 5) Delete an animal\n" +
+				" 6) Quit\n");
+		return waitForInt("Please choose an option: ");
 	}
 
 	public int waitForInt(String prompt) {
@@ -32,6 +52,34 @@ public class MenuService {
 	public String waitForString(String prompt) {
 		System.out.printf(prompt);
 		return scanner.next();
+	}
+
+	public boolean isYesOrNo(String prompt) {
+		String response = waitForString(prompt);
+		if(response.toLowerCase().equals("y")||response.toLowerCase().equals("yes")){
+			return true;
+		}else if(response.toLowerCase().equals("n")||response.toLowerCase().equals("no")){
+			return false;
+		}else{
+			return isYesOrNo(prompt);
+		}
+	}
+
+	public void listAnimals(ArrayList<Animal> animals) {
+		System.out.println("\n-- List Animals --\n");
+		for(int index = 0; index < animals.size();index++){
+			System.out.format(textAlign,(index+1)+")"+animals.get(index).getName(),animals.get(index).getSpecies());
+
+		}
+	}
+
+	public void showDetailsOfAnimal(Animal animal) {
+		System.out.println();
+		System.out.format(textAlign,"Name:",animal.getName());
+		System.out.format(textAlign,"Species:",animal.getSpecies());
+		System.out.format(textAlign,"Breed:",animal.getBreed());
+		System.out.format(textAlign,"Description:",animal.getDescription());
+
 	}
 }
 
--- test/com/tiy/MenuServiceTest.java
+++ test/com/tiy/MenuServiceTest.java
@@ -3,11 +3,13 @@ package com.tiy;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.awt.*;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.io.PrintStream;
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 import static org.hamcrest.CoreMatchers.*;
@@ -23,6 +25,26 @@ public class MenuServiceTest {
 		this.outputStream = new ByteArrayOutputStream();
 		PrintStream printStream = new PrintStream(outputStream);
 		System.setOut(printStream);
+	}
+	@Test
+	public void testForPublicStaticFinalProperties(){
+		String input = "";
+		Scanner scanner = new Scanner(input);
+		MenuService menuService = new MenuService(scanner);
+
+		int list = menuService.LIST_ANIMALS;
+		int create = menuService.CREATE_ANIMAL;
+		int view = menuService.VIEW_ANIMAL;
+		int edit = menuService.EDIT_ANIMAL;
+		int del = menuService.DELETE_ANIMAL;
+		int quit = menuService.QUIT;
+
+		assertThat(list,equalTo(1));
+		assertThat(create,equalTo(2));
+		assertThat(view,equalTo(3));
+		assertThat(edit,equalTo(4));
+		assertThat(del,equalTo(5));
+		assertThat(quit,equalTo(6));
 	}
 
 	@Test
@@ -56,11 +78,95 @@ public class MenuServiceTest {
 	}
 
 	@Test
-	public void checkYesOrNo(){
+	public void checkYesOrNoReturnTrue(){
+		String input = "yes";
+		Scanner scanner = new Scanner(input);
+		MenuService menuService = new MenuService(scanner);
+		boolean response = menuService.isYesOrNo("Would you like to exit(yes/no): ");
+		assertThat(outputStream.toString(),containsString("Would you like to exit(yes/no):"));
+		assertThat(response,equalTo(true));
+	}
 
+	@Test
+	public void checkYesOrNoReturnFalse(){
+		String input = "no";
+		Scanner scanner = new Scanner(input);
+		MenuService menuService = new MenuService(scanner);
+		boolean response = menuService.isYesOrNo("Would you like to exit(yes/no): ");
+		assertThat(outputStream.toString(),containsString("Would you like to exit(yes/no):"));
+		assertThat(response,equalTo(false));
 	}
 	@Test
 	public void testPromptForMenu(){
+		String input = "3";
+		Scanner scanner = new Scanner(input);
+		MenuService menuService = new MenuService(scanner);
+
+		int response = menuService.promptForMainMenu();
+
+		assertThat(outputStream.toString(), containsString("-- Main Menu --"));
+		assertThat(outputStream.toString(), containsString("1) List animals"));
+		assertThat(outputStream.toString(), containsString("2) Create an animal"));
+		assertThat(outputStream.toString(), containsString("3) View animal details"));
+		assertThat(outputStream.toString(), containsString("4) Edit an animal"));
+		assertThat(outputStream.toString(), containsString("5) Delete an animal"));
+		assertThat(outputStream.toString(), containsString("6) Quit"));
+		assertThat(response, equalTo(3));
+	}
+
+	@Test
+	public void testShowListOfAnimalsCheckHeader(){
+		String input = "";
+		Scanner scanner = new Scanner(input);
+		MenuService menuService = new MenuService(scanner);
+		ArrayList<Animal> animals = new ArrayList<>();
+		animals.add(new Animal("",
+				"",
+				"",
+				""));
+
+		menuService.listAnimals(animals);
+
+		assertThat(outputStream.toString(),containsString("-- List Animals --"));
+	}
+	@Test
+	public void testShowListOfAnimals(){
+		String input = "";
+		Scanner scanner = new Scanner(input);
+		MenuService menuService = new MenuService(scanner);
+		ArrayList<Animal> animals = new ArrayList<>();
+		animals.add(new Animal("Bob",
+				"Cat",
+				"na",
+				"Just really fat"));
+
+		menuService.listAnimals(animals);
+
+		assertThat(outputStream.toString(),containsString("-- List Animals --"));
+		assertThat(outputStream.toString(),containsString("1)"));
+		assertThat(outputStream.toString(),containsString("Bob"));
+		assertThat(outputStream.toString(),containsString("Cat"));
+	}
+
+	@Test
+	public void testShowingOfAnimalDetails(){
+		String input = "" ;
+		Scanner scanner = new Scanner(input);
+		MenuService menuService = new MenuService(scanner);
+		Animal animal = new Animal("Bob",
+									"Cat",
+									"N/A",
+									"Fat and likes to Eat.");
+		menuService.showDetailsOfAnimal(animal);
+
+		assertThat(outputStream.toString(),containsString("Name:"));
+		assertThat(outputStream.toString(),containsString(animal.getName()));
+		assertThat(outputStream.toString(),containsString("Species:"));
+		assertThat(outputStream.toString(),containsString(animal.getSpecies()));
+		assertThat(outputStream.toString(),containsString("Breed:"));
+		assertThat(outputStream.toString(),containsString(animal.getBreed()));
+		assertThat(outputStream.toString(),containsString("Description"));
+		assertThat(outputStream.toString(),containsString(animal.getDescription()));
 
 	}
 
